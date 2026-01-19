1f64dc47e07a21437021050c1ee8f7f645d2082d
Cyboring Technologies <dev@cyboring.com>
2026-01-19 11:06:23 -0600

    chore: switch astro to static output for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
   integrations: [tailwind()],
 });
